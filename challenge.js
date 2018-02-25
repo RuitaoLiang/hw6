7942e4780df57347e611bd344f4e731f77bfb1ed
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-02-25 14:35:31 -0800

    Create challenge.js
    easing.js-- using a technique called the "exponential moving average"

---
+++ challenge.js
@@ -0,0 +1,14 @@
+var rate = 0.1;
+var x = 0;
+var y =0;
+function setup(){
+	createCanvas(400,600);
+
+}
+
+function draw() { 
+	background(0);
+  x = mouseX*rate + x*(1-rate); // rate is 0.1, or 10% -- (1-rate) is 0.9, or 90%
+	y = mouseY*rate + y*(1-rate);
+		ellipse(x, y, 15, 15);
+}
